7216239f132891e75035aa62b3b877e91b48cb77
Your Name <you@example.com>
2025-03-27 18:55:20 +0000

    new try

--- list.c
+++ list.c
@@ -67,7 +67,15 @@ void *lastList(List *list) {
   return list->current->data;
 }
 
-void *prevList(List *list) { return NULL; }
+void *prevList(List *list) { return NULL; {
+    Node *aux = list->head;
+    while (aux->next != list->current) {
+      aux = aux->next;
+    }
+    list->current = aux;
+    return list->current->data;
+}
+
 
 void pushFront(List *list, void *data) {}
 
